9d9f948b7bf40847f76a330c025947b003290544
JayMuchoki <119471935+JayMuchoki@users.noreply.github.com>
2024-05-15 09:19:09 +0300

    Update object.js

--- task1/object.js
+++ task1/object.js
@@ -7,3 +7,13 @@ console.log(obj[1]);
 console.log(obj['1']);
 console.log(obj[1] === obj['1']);
 
+// the output will be :
+//String One
+//String One
+//true
+
+// this is because :obj[1] = 'One'; in this even though 1 is a number, it is used as a property key, and JavaScript automatically converts it to a string. hence its output as String one
+
+//obj['1'] = 'String One';: This line adds another property to the obj object with key '1' (a string) and value 'String One'.
+
+//When we check if obj[1] === obj['1'], we are comparing the same item inside the box, so it's true that they are equal.
